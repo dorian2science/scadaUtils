7af7692c6bfcbf75999be44136d4d4104c77604c
dorianDrevon <dorian.drevon@sylfen.com>
2021-09-30 09:18:24 +0200

    dash==1.20.0 to get hovertools back on graph again

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ packages=setuptools.find_packages(),
 package_data={'': ['conf/*']},
 include_package_data=True,
 install_requires=['IPython','matplotlib','scipy','pandas==1.3.1',
-                    'dash==1.21.0','dash_bootstrap_components==0.13.0','flask_caching','psycopg2-binary',
+                    'dash==1.20.0','dash_bootstrap_components==0.13.0','flask_caching','psycopg2-binary',
                     'pyModbusTCP==0.1.10'],
 python_requires=">=3.8"
 )
