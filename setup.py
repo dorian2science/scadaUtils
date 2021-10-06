7164807c27f7bdee8ec31342aa93fb31f5ed0af4
dorianDrevon <dorian.drevon@sylfen.com>
2021-10-06 11:49:22 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ dirParent = "dorianUtils"
 
 setuptools.setup(
 name="dorianUtilsModulaire", # Replace with your own username
-version="3.8.4",
+version="3.8.5",
 author="Dorian Drevon",
 author_email="drevondorian@gmail.com",
 description="Utilities package",
