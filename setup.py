dfa47b37a04992ee0f2a62d491120c6ab4a1f284
dorianDrevon <dorian.drevon@sylfen.com>
2021-12-20 21:35:35 +0100

    pymodbus==2.5.3 added in setup.py

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ package_data={'': ['conf/*']},
 include_package_data=True,
 install_requires=['IPython','matplotlib','scipy','pandas==1.3.1',
                     'dash==1.20.0','dash-daq==0.5.0','dash_bootstrap_components==0.13.0','flask_caching','psycopg2-binary',
-                    'pyModbusTCP==0.1.10','opcua==0.98.9','cryptography==2.8'],
+                    'pymodbus==2.5.3','opcua==0.98.9','cryptography==2.8'],
 python_requires=">=3.8"
 )
