589d08188b17da0075ce1b6e81cc1c253c7098c4
dorianDrevon <dorian.drevon@sylfen.com>
2022-01-14 18:00:35 +0100

    setup.py dependencies updated

--- setup.py
+++ setup.py
@@ -26,8 +26,9 @@ classifiers=[
 packages=setuptools.find_packages(),
 package_data={'': ['conf/*']},
 include_package_data=True,
-install_requires=['IPython','matplotlib','scipy','pandas==1.3.1',
-                    'dash==1.20.0','dash-daq==0.5.0','dash_bootstrap_components==0.13.0','flask_caching','psycopg2-binary',
-                    'pymodbus==2.5.3','opcua==0.98.13','cryptography==2.8'],
+install_requires=['IPython==7.20.0','pandas==1.3.1',
+                    'dash==1.20.0','dash-daq==0.5.0','dash_bootstrap_components==0.13.0',
+                    'flask_caching','psycopg2-binary','pymodbus==2.5.3','opcua==0.98.13',
+                    'cryptography==2.8','odfpy==1.4.1'],
 python_requires=">=3.8"
 )
