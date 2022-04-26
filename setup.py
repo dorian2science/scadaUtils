1bd8b099957805febea083f90ef68da7ba58ef83
dorianDrevon <dorian.drevon@sylfen.com>
2022-04-26 14:10:02 +0200

    add plotly dependency in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers=[
 packages=setuptools.find_packages(),
 package_data={'': ['conf/*']},
 include_package_data=True,
-install_requires=['IPython','pandas>=1.3.1','psycopg2-binary','odfpy==1.4.1',
+install_requires=['IPython','pandas>=1.3.1','psycopg2-binary','odfpy==1.4.1','plotly>=5.5.0',
     'pymodbus==2.5.3','opcua==0.98.13','cryptography==2.8','Pillow==7.0.0'],
 python_requires=">=3.8"
 )
