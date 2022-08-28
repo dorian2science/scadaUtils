9098807f55a1673d084c6fc79606b284a56b9845
dorianDrevon <dorian.drevon@sylfen.com>
2022-08-29 01:51:38 +0200

    correct setup.py to include static and templates files.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 name="sylfenUtils",
-version="1.0.0",
+version="1.1",
 author="Dorian Drevon",
 author_email="drevondorian@gmail.com",
 description="Utilities package",
@@ -17,7 +17,7 @@ classifiers=[
     "Operating System :: OS Independent",
 ],
 packages=setuptools.find_packages(),
-package_data={'': ['conf/*','static/','templates/']},
+package_data={'': ['conf/*','static/lib/*','templates/*']},
 include_package_data=True,
 install_requires=['IPython','pandas==1.3.1','psycopg2-binary','odfpy==1.4.1','plotly>=5.5.0',
     'pymodbus==2.5.3','opcua==0.98.13','cryptography==2.8','Pillow==7.0.0','openpyxl==3.0.7',
