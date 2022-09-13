0c4ca8d867f95405e24183cb5754c28ad26d335b
dorianDrevon <dorian.drevon@sylfen.com>
2022-09-13 17:02:45 +0200

    1.9 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 name="sylfenUtils",
-version="1.8",
+version="1.9",
 author="Dorian Drevon",
 author_email="drevondorian@gmail.com",
 description="Utilities package",
