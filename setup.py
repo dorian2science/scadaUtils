e5a174f19b8d002aeaf64bf6fd5139191a0eb2a8
dorianDrevon <dorian.drevon@sylfen.com>
2023-05-30 15:01:48 +0200

    1.19.2 official

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 base_version=re.search('(SylfenUtils V)(\d+\.\d*)',long_description).groups()[1]
-v_try='1'
+v_try='2'
 
 setuptools.setup(
     name="sylfenUtils",
