1289a8e9cb677fcc576a20330f784c6477c1aa24
Dorian Inocel <dorian.drevon@inocel.com>
2023-12-01 16:18:29 +0000

    set

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 base_version=re.search('(ScadaUtils V)(\d+\.\d*)',long_description).groups()[1]
-v_try='x'
+v_try='2'
 
 setuptools.setup(
     name="scadaUtils",
